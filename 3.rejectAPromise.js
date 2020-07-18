349fd81e72989bc4de4f2fb4f6ae3a3c036e7221
Imilla Dev <lupemaydanda@gmail.com>
2020-07-18 19:31:42 -0400

    style: Remove comments

--- 3.rejectAPromise.js
+++ 3.rejectAPromise.js
@@ -1,12 +1,9 @@
 var promise = new Promise(function (fulfill, reject) {
-  // Your solution here
   setTimeout(() => reject(new Error("REJECTED!")), 300);
 });
 
 function onReject(error) {
-  // Your solution here
   console.log(error.message);
 }
 
-// Your solution here
 promise.then(undefined, (error) => onReject(error));
